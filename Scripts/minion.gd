9b1c48a72ee13d9b214495eedd55f9da42fc14b0
lasive <109923383+lasive@users.noreply.github.com>
2024-04-13 21:22:28 +0300

    added minion Walk animation

--- Scripts/minion.gd
+++ Scripts/minion.gd
@@ -3,7 +3,10 @@ class_name Minion extends Character
 @export var speed: float = 9.5
 @export var path_calc_time: float = 0.1
 @export var max_distance_from_master: float = 4.0
+@export var model: Node3D = null
+@onready var x_scale = model.scale.x
 var elapsed_time: float = 0
+var anim_player: AnimationPlayer = null
 
 @onready var _navigation_agent: NavigationAgent3D = $NavigationAgent3D
 
@@ -20,6 +23,9 @@ var _follow_node: FollowNode = null
 func _ready() -> void:
 	_navigation_agent.target_desired_distance = 2.5
 	_navigation_agent.path_desired_distance = 2
+	for child in model.get_children():
+		if child is AnimationPlayer:
+			anim_player = child
 
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
@@ -33,19 +39,28 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 			direction.y = 0
 			direction = direction.normalized()
 			linear_velocity = direction * speed
+			model.scale.x = x_scale * sign(direction.x)
 		else:
+			anim_player.pause()
 			# Walk with master
 			_state = State.WALK_WTIH_MASTER
 	elif _state == State.WALK_WTIH_MASTER:
 		var input_dir := Input.get_vector("left", "right", "up", "down")
 		var movement_velocity := Vector3(input_dir.x, 0, input_dir.y) * speed
 		linear_velocity = movement_velocity
+		
+		if movement_velocity.length() > 0:
+			anim_player.play("Walk")
+		else:
+			anim_player.pause()
+			
 		if global_position.distance_to(_current_master.global_position) > \
 		max_distance_from_master:
 			get_close_to_master(_current_master)
 		# need to check if is too far from master
 	else:
 		linear_velocity = Vector3.ZERO
+		anim_player.pause()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -59,6 +74,7 @@ func _process(delta: float) -> void:
 
 func set_follow_node(follow_node: FollowNode) -> void:
 	_follow_node = follow_node
+	
 
 
 func get_close_to_master(master: RigidBody3D) -> void:
@@ -66,4 +82,5 @@ func get_close_to_master(master: RigidBody3D) -> void:
 	_current_master = master
 	elapsed_time = 0
 	_navigation_agent.target_position = _current_master.global_position
+	anim_player.play("Walk")
 	
