0409e2796b173caee5f33ba903742503a5408b6d
AndrejLapin <lapin.andrej1@gmail.com>
2024-04-13 20:39:19 +0300

    spawner changes

--- Scripts/grunt.gd
+++ Scripts/grunt.gd
@@ -7,28 +7,46 @@ var elapsed_time: float = 0
 
 @onready var _navigation_agent: NavigationAgent3D = $NavigationAgent3D
 
-var _target: Node3D = null
-
 enum State {
-	
+	IDLE,
+	GOING_TO_TARGET,
+	ATTACKING
 }
+var _state: State = State.IDLE
+var _target: Node3D = null
+var _move_direction: Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_navigation_agent.target_desired_distance = 2.5
-	_navigation_agent.path_desired_distance = 2
+	_navigation_agent.path_desired_distance = 2.0
 	if _target:
 		_navigation_agent.target_position = _target.global_position
 
 
+func _physics_process(delta: float) -> void:
+	if _state == State.IDLE:
+		pass
+	elif _state == State.GOING_TO_TARGET:
+		_process_going_to_target(delta)
+	elif _state == State.ATTACKING:
+		pass
+
+
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	linear_velocity = _move_direction * speed
+
+
+func _process_going_to_target(delta: float) -> void:
 	if not _navigation_agent.is_navigation_finished():
 		var nex_path_position := _navigation_agent.get_next_path_position()
 		var direction := global_position.direction_to(nex_path_position)
 		direction.y = 0
-		direction = direction.normalized()
-		linear_velocity = direction * speed
+		_move_direction = direction.normalized()
 	else:
+		_move_direction = Vector3.ZERO
+		_state = State.ATTACKING
+		#check range and set to attack
 		pass
 
 
@@ -42,5 +60,6 @@ func _process(delta: float) -> void:
 func set_target(target: Node3D) -> void:
 	elapsed_time = 0
 	_target = target
+	_state = State.GOING_TO_TARGET
 	if _navigation_agent:
 		_navigation_agent.target_position = _target.global_position
