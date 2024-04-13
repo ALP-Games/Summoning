958b2b7c8a28e5d5294ef5a40a9d1e5d459e9b64
AndrejLapin <lapin.andrej1@gmail.com>
2024-04-13 12:13:02 +0300

    basic movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,6 +7,12 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var movement_velocity := Vector3(input_dir.x, 0, input_dir.y) * speed
+	linear_velocity = movement_velocity
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
