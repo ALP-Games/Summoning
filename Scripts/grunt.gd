62f063fb37d8351f96458bd492f41eddc0f3ffae
AndrejLapin <lapin.andrej1@gmail.com>
2024-04-13 20:05:32 +0300

    enemy spawner

--- Scripts/grunt.gd
+++ Scripts/grunt.gd
@@ -1,4 +1,4 @@
-class_name Minion extends Character
+class_name Grunt extends Character
 
 @export var speed: float = 9.5
 @export var path_calc_time: float = 0.1
@@ -7,63 +7,40 @@ var elapsed_time: float = 0
 
 @onready var _navigation_agent: NavigationAgent3D = $NavigationAgent3D
 
+var _target: Node3D = null
+
 enum State {
-	GET_CLOSE_TO_MASTER,
-	WALK_WTIH_MASTER,
-	STAY
+	
 }
-var _state: State = State.STAY
-var _current_master: RigidBody3D = null
-var _follow_node: FollowNode = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_navigation_agent.target_desired_distance = 2.5
 	_navigation_agent.path_desired_distance = 2
+	if _target:
+		_navigation_agent.target_position = _target.global_position
 
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	if _state == State.GET_CLOSE_TO_MASTER:
-		if _current_master == null:
-			# get new master or become idle
-			pass
-		if not _navigation_agent.is_navigation_finished():
-			var nex_path_position := _navigation_agent.get_next_path_position()
-			var direction := global_position.direction_to(nex_path_position)
-			direction.y = 0
-			direction = direction.normalized()
-			linear_velocity = direction * speed
-		else:
-			# Walk with master
-			_state = State.WALK_WTIH_MASTER
-	elif _state == State.WALK_WTIH_MASTER:
-		var input_dir := Input.get_vector("left", "right", "up", "down")
-		var movement_velocity := Vector3(input_dir.x, 0, input_dir.y) * speed
-		linear_velocity = movement_velocity
-		if global_position.distance_to(_current_master.global_position) > \
-		max_distance_from_master:
-			get_close_to_master(_current_master)
-		# need to check if is too far from master
+	if not _navigation_agent.is_navigation_finished():
+		var nex_path_position := _navigation_agent.get_next_path_position()
+		var direction := global_position.direction_to(nex_path_position)
+		direction.y = 0
+		direction = direction.normalized()
+		linear_velocity = direction * speed
 	else:
-		linear_velocity = Vector3.ZERO
+		pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	elapsed_time += delta
-	if elapsed_time >= path_calc_time and \
-	_current_master and _state == State.GET_CLOSE_TO_MASTER:
+	if elapsed_time >= path_calc_time:
 		elapsed_time -= path_calc_time
-		_navigation_agent.target_position = _current_master.global_position
+		_navigation_agent.target_position = _target.global_position
 
 
-func set_follow_node(follow_node: FollowNode) -> void:
-	_follow_node = follow_node
-
-
-func get_close_to_master(master: RigidBody3D) -> void:
-	_state = State.GET_CLOSE_TO_MASTER
-	_current_master = master
+func set_target(target: Node3D) -> void:
 	elapsed_time = 0
-	_navigation_agent.target_position = _current_master.global_position
-	
+	_target = target
+	if _navigation_agent:
+		_navigation_agent.target_position = _target.global_position
--- Scripts/grunt_spawner.gd
+++ Scripts/grunt_spawner.gd
@@ -2,6 +2,7 @@ class_name GruntSpawner extends Building
 
 @export var spawn_interval: float = 5.0
 @export var grunt: PackedScene = null
+@export var spawn_offset: Vector3 = Vector3(0.0, 0.0, 3.0)
 
 var elapsed_time: float = 0.0
 
@@ -15,5 +16,11 @@ func _process(delta: float) -> void:
 	elapsed_time += delta
 	if elapsed_time >= spawn_interval:
 		elapsed_time -= spawn_interval
-		var grunt_instance = grunt.instantiate()
+		var grunt_instance := grunt.instantiate() as Grunt
+		grunt_instance.set_target(game_manager.gameplay.spirit_well)
 		game_manager.gameplay.entities.add_child(grunt_instance)
+		var spawn_pos := global_position
+		spawn_pos.y = 0.0
+		grunt_instance.global_position = spawn_pos + spawn_offset
+		#grunt_instance.global_position += spawn_offset
+	
